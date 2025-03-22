f337b9c6fc81cd1a67b3e7a27ab2a3a5c0448f81
Muhammad Juniarto <53488921+WallNutss@users.noreply.github.com>
2025-03-22 13:50:44 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,9 +27,9 @@ cp ~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app
 
 # Zip the folder
 zip_cmd="zip $output_dir/biometric-esp32-group-c.zip"
-[ -f "$platformio_build_dir/bootloader.bin" ] && zip_cmd="$zip_cmd bootloader.bin"
-[ -f "partitions.bin" ] && zip_cmd="$zip_cmd partitions.bin"
-[ -f "$platformio_build_dir/firmware.bin" ] && zip_cmd="$zip_cmd firmware.bin"
-[ -f "$platformio_build_dir/spiffs.bin" ] && zip_cmd="$zip_cmd spiffs.bin"
-[ -f "~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin" ] && zip_cmd="$zip_cmd boot_app0.bin"
+[ -f "$output_dir/bootloader.bin" ] && zip_cmd="$zip_cmd $output_dir/bootloader.bin"
+[ -f "$output_dir/partitions.bin" ] && zip_cmd="$zip_cmd $output_dir/partitions.bin"
+[ -f "$output_dir/firmware.bin" ] && zip_cmd="$zip_cmd $output_dir/firmware.bin"
+[ -f "$output_dir/spiffs.bin" ] && zip_cmd="$zip_cmd $output_dir/spiffs.bin"
+[ -f "$output_dir/boot_app0.bin" ] && zip_cmd="$zip_cmd $output_dir/boot_app0.bin"
 eval $zip_cmd
